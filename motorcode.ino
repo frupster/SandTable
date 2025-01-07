174cc811ecef1ca47b5349319cc0e991098c802b
frupster <117213809+frupster@users.noreply.github.com>
2025-01-06 22:55:46 -0500

    current motor file

--- motorcode.ino
+++ motorcode.ino
@@ -1,471 +1,415 @@
-#include <Wire.h>
-#include <Keypad.h>
 #include <ezButton.h>
+#include <Wire.h>
+int num = 1;
+int colour = 0;  //leds
+int button = 0;
+int delayMin = 1;
+bool homing = false;
+int change = 0;
+int power = 1;
+int pause = 0;
+//int right=0;
+//int arrayLength = 0;
+//float slope = 0;
+float m = 0;
+float distance = 1;
+float defaultDistance = 0.2;
 
-char x;
-String room = "";
-const int StepX = 2;
-const int DirX = 5;
-const int StepY = 3;
-const int DirY = 6;
-const int rotations = 15;
-const double constant = 3.56;
+
+int dx = 10; // for bounce
+int dy = 5;
 
 
+int width = 21;
+int height = 23;
+int buttonStateX = 1;
+int buttonStateY = 1;
+//x , y
 
 
-ezButton limitSwitch1(9);
-ezButton limitSwitch2(10);
+//math diameter 15mm
+// ONe rotation = 1600 pulses
+//int incomingByte = 0;
+// defines pins
+#define stepPin 2  //x
+#define dirPin 5   //x
+#define enPin 8    //
+#define stepPin2 3  //y
+#define dirPin2 6   //y
+#define green A0  
+#define blue A1  
+#define red A2
+#define xSwitch 9
+#define ySwitch 10
+int speed = 1600;
+float rotations = 15;  //15mm per rotations = 1600 pulses
+float x = 0;
+float y = 0;
+//int x1, y1, x2, y2 = 0;
+int ranNum = 0;
+//in mm
+double turns = (distance / rotations) * 1600;
 
 
-void setup() {
+#define SLAVE_ADDRESS 9  // I2C address for this slave
 
-  pinMode(StepX, OUTPUT);
-  pinMode(DirX, OUTPUT);
-  pinMode(StepY, OUTPUT);
-  pinMode(DirY, OUTPUT);
+void setup() {  
+  Serial.println("setup");
+    Serial.begin(9600);
+pinMode(LED_BUILTIN, OUTPUT);
 
-  pinMode(8, OUTPUT);
+  pinMode(green, OUTPUT);
+  pinMode(red, OUTPUT);
+  pinMode(blue, OUTPUT);
+  // Sets the two pins as Outputs
+  pinMode(stepPin, OUTPUT);
+  pinMode(dirPin, OUTPUT);
+  pinMode(stepPin2, OUTPUT);
+  pinMode(dirPin2, OUTPUT);
+  pinMode(enPin, OUTPUT);
+  pinMode(ySwitch, INPUT_PULLUP);
+  pinMode(xSwitch, INPUT_PULLUP);
+  digitalWrite(enPin, LOW);
 
-  digitalWrite(8, LOW);
-  // Define the LED pin as Output
-  pinMode(LED_BUILTIN, OUTPUT);
-  // Start the I2C Bus as Slave on address 9
-  Wire.begin(9);
-  // Attach a function to trigger when something is received.
-  Wire.onReceive(receiveEvent);
-  Serial.begin(9600);
+
+  digitalWrite(green, HIGH);
+  digitalWrite(red, HIGH);
+  digitalWrite(blue, HIGH);
+
+  Wire.begin(SLAVE_ADDRESS);  // Initialize I2C as slave with address 9
+  Wire.onReceive(receiveEvent);  // Register a function to handle received data
+
+  Serial.println("finished setup");
+
+  //home();
+}
+
+void receiveEvent(int bytesReceived) {
+  String receivedData = "";  // Variable to store the received data
+
+  while (Wire.available()) {
+    char c = Wire.read();  // Read one byte from the I2C bus
+    receivedData += c;     // Append the received character to the string
+  }
+
+  //Serial.print("Received: ");
+  Serial.println(receivedData); 
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(100);
   digitalWrite(LED_BUILTIN, LOW);
-  Serial.println("setup done");
-}
-
-void receiveEvent(int bytes) {
-  Serial.println("detected input");
-  x = Wire.read();
-  if (x == 'R') {
-    resetRoom();
-  } else {
-    room += x;
-
-  }
-}
-
-void loop() {
-  //Serial.println(room);
-
-
-if(room[0] == 'A'){
-if(room == "A0"){
-    home();
-    delay(3000);
-    moveLeft(6);
-    moveUp(9);
-    moveLeft(13.5);
-    moveUp(20.5);
-    moveLeft(18);
-    moveUp(8);
-    resetRoom();
-    return;
-    }
-    room.remove(0, 1);
-
-  int temp = (room.toInt()/10);
-
-    if(temp>= 116 && temp<= 124){
-      home();
-      delay(3000);
-    moveLeft(6);
-    moveUp(9);
-    moveLeft(13.5);
-    moveUp(20.5);
-    moveLeft(10);
-    
-    resetRoom();
-    return;
-    }
-
-    if(temp>=101 && temp<=110){
-       home();
-      delay(3000);
-    moveLeft(6);
-    moveUp(9);
-    moveLeft(13.5);
-    moveUp(34);
-    moveLeft(10);
-    
-    resetRoom();
-    return;
-    }
-    
-if(temp>= 111 && temp<= 115){
-    home();
-    delay(3000);
-    moveLeft(6);
-    moveUp(9);
-    moveLeft(13.5);
-    moveUp(20.5);
-    moveLeft(18);
-    moveUp(8);
-    resetRoom();
-    return;
-    }
-    
-    
+  interpretInput(receivedData); // Print the received data to the Serial Monitor
+  //home();
 
 }
-
-else if(room[0] == 'B'){
-  if(room == "B0"){
-     home();
-    delay(3000);
-    moveLeft(6);
-    moveUp(9);
-    moveLeft(5.5);
-    moveUp(20.5);
-    moveRight(17);
-    moveUp(8);
-    resetRoom();
-    return;
-  }
-  room.remove(0, 1);
-
-  int temp = (room.toInt()/10);
-
-    if(temp>= 116 && temp<= 124){
-    home();
-    delay(3000);
-    moveLeft(6);
-    moveUp(9);
-    moveLeft(5.5);
-    moveUp(20.5);
-    moveRight(9);
-    resetRoom();
-    return;
-    
-    }
-
-    if(temp>=101 && temp<=110){
-     home();
-    delay(3000);
-    moveLeft(6);
-    moveUp(9);
-    moveLeft(5.5);
-    moveUp(35);
-    moveRight(14);
-    resetRoom();
-    return;
-     
-    }
-    
-if(temp>= 111 && temp<= 115){
-    home();
-    delay(3000);
-    moveLeft(6);
-    moveUp(9);
-    moveLeft(5.5);
-    moveUp(20.5);
-    moveRight(17);
-    moveUp(8);
-    resetRoom();
-    return;
-    
-    }
-    
-}
-
-else if(room[0] == 'C'){
-  if(room == "C0"){
-    home();
-    delay(3000);
-    moveLeft(6);
-    moveUp(9);
-    moveLeft(5.5);
-    moveUp(33);
-    moveLeft(5);
-    moveUp(5);
-    delay(3000);
-    moveUp(39);
-    delay(3000);
-    moveDown(5);
-    moveLeft(20);
-    moveDown(9);
-    resetRoom();
-    return;
-  }
-
-  room.remove(0, 1);
-
-  int temp = (room.toInt()/10);
-
-    if(temp>= 116 && temp<= 124){
-    
-    home();
-    delay(3000);
-    moveLeft(6);
-    moveUp(9);
-    moveLeft(5.5);
-    moveUp(33);
-    moveLeft(5);
-    moveUp(5);
-    delay(3000);
-    moveUp(39);
-    delay(3000);
-    moveDown(5);
-    moveLeft(5);
-    moveDown(14);
-    moveLeft(15);
-    resetRoom();
-    return;
-    }
-
-    if(temp>=101 && temp<=110){
-     home();
-    delay(3000);
-    moveLeft(6);
-    moveUp(9);
-    moveLeft(5.5);
-    moveUp(33);
-    moveLeft(5);
-    moveUp(5);
-    delay(3000);
-    moveUp(39);
-    delay(3000);
-    moveDown(5);
-    moveLeft(10);
-    resetRoom();
-    return;
-     
-    }
-    
-if(temp>= 111 && temp<= 115){
-    home();
-    delay(3000);
-    moveLeft(6);
-    moveUp(9);
-    moveLeft(5.5);
-    moveUp(33);
-    moveLeft(5);
-    moveUp(5);
-    delay(3000);
-    moveUp(39);
-    delay(3000);
-    moveDown(5);
-    moveLeft(20);
-    moveDown(9);
-    resetRoom();
-    return;
-    
-    }
-}
-
-else if(room[0] == 'D'){
-  if(room == "D0"){
-    home();
-    delay(3000);
-    moveLeft(6);
-    moveUp(9);
-    moveLeft(5.5);
-    moveUp(33);
-    moveLeft(5);
-    moveUp(5);
-    delay(3000);
-    moveUp(40);
-    delay(3000);
-    moveDown(5);
-    moveRight(20);
-    moveDown(9);
-    resetRoom();
-  }
-
-  room.remove(0, 1);
-
-  int temp = (room.toInt()/10);
-
-    if(temp>= 116 && temp<= 124){
-     home();
-    delay(3000);
-    moveLeft(6);
-    moveUp(9);
-    moveLeft(5.5);
-    moveUp(33);
-    moveLeft(5);
-    moveUp(5);
-    delay(3000);
-    moveUp(40);
-    delay(3000);
-    moveDown(5);
-    moveRight(5);
-    moveDown(15);
-    moveRight(10);
-    resetRoom();
-    
-    }
-
-    if(temp>=101 && temp<=110){
-     
-      home();
-    delay(3000);
-    moveLeft(6);
-    moveUp(9);
-    moveLeft(5.5);
-    moveUp(33);
-    moveLeft(5);
-    moveUp(5);
-    delay(3000);
-    moveUp(40);
-    delay(3000);
-    moveDown(5);
-    moveRight(15);
-    resetRoom();
-    }
-    
-if(temp>= 111 && temp<= 115){
-     home();
-    delay(3000);
-    moveLeft(6);
-    moveUp(9);
-    moveLeft(5.5);
-    moveUp(33);
-    moveLeft(5);
-    moveUp(5);
-    delay(3000);
-    moveUp(40);
-    delay(3000);
-    moveDown(5);
-    moveRight(20);
-    moveDown(9);
-    resetRoom();
-
-    }
-}
-
-else if (room == "9" || room == "90") {
-    home();
-    resetRoom();
-    return;
-  } 
-   else if (room == "1"|| room == "10"){
-    home();
-    delay(3000);
-    moveLeft(5);
-    moveDown(9);
-    moveLeft(19);
-    resetRoom();
-    return;
-  } 
-   else if (room == "2"|| room == "20"){
-    home();
-    delay(3000);
-    moveLeft(5);
-    moveDown(9);
-    moveLeft(8);
-    resetRoom();
-    return;
-
+void interpretInput(String input){
   
-  } 
-   else if (room == "3"|| room == "30"){
+ 
+
+  if(input == "0"){ 
+    down(5);
+  }
+  if(input == "5"){
+    up(5);
+  }
+  if(input == "7"){
+    left(5);
+  }
+  if(input == "9"){
+    right(5);
+  }
+
+  if(input == "2"){
+    circle(5);
+  }
+
+  if(input == "C"){
+    plot(10, 10, 15, 15);
+  }
+  if(input == "D"){
+    plot(10, 10, 5, 15);
+  }
+
+  if(input == "1"){
+    up(1);
+    delay(100000);
+    down(1);
+    delay(100000);
+    left(1);
+    delay(100000);
+    right(1);
+  }
+
+  if(input == "4"){
+    plot(x, y, 0, height);
+  }
+ 
+  if(input == "8"){
     home();
-    delay(3000);
-    moveLeft(5);
-    moveDown(9);
-    moveLeft(19);
-    moveUp(10);
-    resetRoom();
-    return;
-  } 
-   else if (room == "4" || room == "40"){
-    home();
-    delay(3000);
-    moveLeft(6);
-    moveUp(10);
-    moveLeft(10);
-       resetRoom();
-    return;
+  }
+
+  if(input == "A"){
+    leftRight();
+  }
+  if(input == "B"){
+    String xStr = String(x);
+    String yStr = String(y);
+    Serial.println("(" + xStr + ", " + yStr + ")");
+  }
+
+}
+
+
+void home(){
+  buttonStateY = digitalRead(ySwitch);
+  digitalWrite(dirPin, LOW);    
+  digitalWrite(dirPin2, HIGH);  //moves down until limit switch is triggered
+  while(buttonStateY == 1){
+
+    digitalWrite(stepPin, HIGH);
+    digitalWrite(stepPin2, HIGH);
+    delayMicroseconds(speed);
+    digitalWrite(stepPin, LOW);
+    digitalWrite(stepPin2, LOW);
+    delayMicroseconds(speed);
+    buttonStateY = digitalRead(ySwitch);
+  }
+  buttonStateX = digitalRead(xSwitch);
+
+  digitalWrite(dirPin, HIGH);   
+  digitalWrite(dirPin2, HIGH);  //moves right until limit switch is triggered
+  
+  while(buttonStateX == 1){
+  digitalWrite(stepPin, HIGH);
+  digitalWrite(stepPin2, HIGH);
+  delayMicroseconds(speed);
+  digitalWrite(stepPin, LOW);
+  digitalWrite(stepPin2, LOW);
+  delayMicroseconds(speed);
+  buttonStateX = digitalRead(xSwitch);
+  }
+  x=21;
+  y=0;
+
+}
+
+void loop(){
+  /*
+   if(digitalRead(ySwitch) == 0){
+    Serial.println("ySwitch");
+   }
+   if(digitalRead(xSwitch) == 0){
+    Serial.println("xSwitch");
+   }
+   */
+}
+
+void leftRight()  //clear
+{
+  home(); // (21, 0)
+  float originalValue = speed;
+  speed = 800;
+  for(int i = 0; i < height; i++){
+    if(x < 5){
+      plot(x, y, width, i);
+    }
+    else if(x == width){
+      plot(x, y, 0, i);
+    }
+    up(1);
+  }
+  speed = originalValue;
+}
+void circle(float r) {
+  // Start at the midpoint and go up by the radius
+  float angleStep = 1; // Increment in degrees for plotting points (controls resolution)
+  float xOrigin = x;
+  float yOrigin = y;
+  // Loop to draw points around the circle
+  for (float angle = 0; angle < 360; angle += angleStep) {
+    // Convert angle to radians
+    float angleRad = angle * (PI / 180);
+    
+    // Parametric equations for circle
+    float x2 = (r * cos(angleRad)) + xOrigin;  // X coordinate
+    float y2 = (r * sin(angleRad)) + yOrigin;  // Y coordinate
+
+    plot(x, y, x2, y2);
   }
 }
 
-void moveStepper(double distance) {
-  double turns = ((distance / rotations) * 1600 * constant); // Specify the amount of time the steppers will move
-  for (double x = 0; x < turns; x++) { //loops for amount of turns calculated
-    digitalWrite(StepX, HIGH); // Turns on X Stepper Motor
-    digitalWrite(StepY, HIGH); // Turns on Y Stepper Motor
-    delayMicroseconds(250); // Delay 250 microseconds
-    digitalWrite(StepX, LOW); // Turns off X Stepper Motor
-    digitalWrite(StepY, LOW);// Turns off Y Stepper Motor
-    delayMicroseconds(250); // Delay 250 ms
+
+void circle1(float r ){
+  //starts at midpoint, goes up by radius
+  up(r);
+  for(float i = 0; i < r/2; i++){
+    float x2 = sqrt((pow(r,2) - pow(y, 2)));
+    Serial.println(x2);
+    float y2 = sqrt((pow(r,2) - pow(y, 2)));
+    Serial.println(y2);
+    plot(x, y, x2, y2);
   }
 }
 
-void moveRight(double distance) {
-  digitalWrite(DirX, HIGH); //Sets the steppers to turn in the correct direction for the movment
-  digitalWrite(DirY, HIGH); //Sets the steppers to turn in the correct direction for the movment
-  moveStepper(distance); // calls the moveStepper function for the distance specified
-}
+///////////Main plot function between two points
+void plot(int x1, int y1, int x2, int y2) {
+  //if (power==1){home(); return;}
+
+  if (button > 0) { return; }
+  m = (float(y2 - y1) / float(x2 - x1))*defaultDistance;  // y/x  slope
+
+  Serial.print("dy=");
+  Serial.print((y2 - y1));
+  Serial.print(" dx=");
+  Serial.print((x2 - x1));
+
+  Serial.print(" slope=");
+  Serial.print(m);
+  Serial.print(" loc=");
+
+  Serial.print(x);
+  Serial.print(",");
+  Serial.print(y);
+
+  Serial.print(" num=");
+  Serial.print(num);
+  Serial.print(" pow=");
+  Serial.print(power);
+  Serial.print(" button=");
+  Serial.print(button);
+  Serial.print(" ranNum=");
+  Serial.println(ranNum);
+  //Serial.println(button);
+
+  //Serial.print("slope=");
 
 
-void moveLeft(double distance) {
-  digitalWrite(DirX, LOW);
-  digitalWrite(DirY, LOW);
-  moveStepper(distance);
-}
-
-void moveDown(double distance) {
-  digitalWrite(DirX, HIGH);  // Enables the motor to move right
-  digitalWrite(DirY, LOW);   // Enables the motor to move right
-  moveStepper(distance);
-}
-
-void moveUp(double distance) {
-  digitalWrite(DirX, LOW);   // Enables the motor to move right
-  digitalWrite(DirY, HIGH);  // Enables the motor to move right
-  moveStepper(distance);
-}
-void home() {
-
-  digitalWrite(DirY, HIGH);
-  digitalWrite(DirX, HIGH);
-
-  bool state = true;
-  digitalWrite(DirX, HIGH);  // Enables the motor to move right
-  digitalWrite(DirY, LOW);
-
-  while (state == true) {
-    limitSwitch1.loop();  // MUST call the loop() function first
-
-    digitalWrite(StepX, HIGH);
-    digitalWrite(StepY, HIGH);
-    delayMicroseconds(250);
-    digitalWrite(StepX, LOW);
-    digitalWrite(StepY, LOW);
-    delayMicroseconds(250);
-
-    if (limitSwitch1.isPressed())
-      break;
+  while (y + (y2 - y1) > height + 3 || x + (x2 - x1) > width + 2 || x + (x2 - x1) < -2 || y + (y2 - y1) < -2) {
+    Serial.print("outsideRange");
   }
+  if ((x2 - x1 == 0))  // dx=0 up or down.
+  {
+    for (int i = 0; i < abs(y2 - y1); i++)  //x  one// unit in x dir.
+    {
+      //    if (power==1){break;}
+      distance = 1;  //in mm
+      //turns = (distance / rotations) * 1600;
 
-  digitalWrite(DirX, HIGH);
-  digitalWrite(DirY, HIGH);
-  while (state == true) {
-    limitSwitch2.loop();  // MUST call the loop() function first
+      if ((y2 - y1) > 0) {
+        up(distance);
+      }
 
-    digitalWrite(StepX, HIGH);
-    digitalWrite(StepY, HIGH);
-    delayMicroseconds(250);
-    digitalWrite(StepX, LOW);
-    digitalWrite(StepY, LOW);
-    delayMicroseconds(250);
-
-    if (limitSwitch2.isPressed()) {
-
-      break;
+      if (y2 - y1 < 0) {
+        down(distance);
+      }
     }
   }
-  moveUp(38);
-  moveLeft(6);
+  // go on angles
+  else {
+    if (x2 - x1 > 0)  // going right
+    {
+      for (float i = 0; i < abs(x2 - x1); i+= defaultDistance)  //x  one unit in x dir.
+      {
+        distance = defaultDistance;  //in mm
+                       // turns = (distance / rotations) * 1600;
+        right(distance);
+        distance = abs(m);  //in mm in y dir.
+                            // turns = (distance / rotations) * 1600;
+        if (m > 0) {
+          up(distance);
+        }
+
+        if (m < 0) {
+          down(distance);
+        }
+      }
+    }
+
+    if (x2 - x1 < 0)  // going left
+    {
+      for (float i = 0; i < abs(x2 - x1); i+=defaultDistance)  //x  one unit in x dir.
+      {
+        distance = defaultDistance;  //in mm
+                       //   turns = (distance / rotations) * 1600;
+        left(distance);
+        distance = abs(m);  //in mm in y dir.
+                            //   turns = (distance / rotations) * 1600;
+        if (m > 0) {
+          down(distance);
+        }
+
+        if (m < 0) {
+          up(distance);
+        }
+      }
+    }
+  }
 }
 
 
-void resetRoom() {
-  room = "";
+
+
+
+/// Basic Functions
+void stepperMotor(float distance) {
+  turns = (distance / rotations) * 1600;
+  for (int i = 0; i < turns; i++) {
+
+
+    digitalWrite(stepPin, HIGH);
+    digitalWrite(stepPin2, HIGH);
+    delayMicroseconds(speed);
+    digitalWrite(stepPin, LOW);
+    digitalWrite(stepPin2, LOW);
+    delayMicroseconds(speed);
+    while (pause == 1) {
+      //Serial.println("pause");
+    };
+
+
+
+
+    //if (power==1){return;}
+  }
+}
+
+
+
+
+void right(float distance) {
+
+  if (button > 0) { return; }
+  digitalWrite(dirPin, HIGH);   // Enables the motor to move right
+  digitalWrite(dirPin2, HIGH);  // Enables the motor to move right
+  stepperMotor(distance);
+  x = x + distance;
+}
+
+
+void up(float distance) {
+
+  if (button > 0) { return; }
+  digitalWrite(dirPin, HIGH);  // Enables the motor to move right
+  digitalWrite(dirPin2, LOW);  // Enables the motor to move right/HIGH left/LOW
+  stepperMotor(distance);
+  y = y + distance;
+}
+
+
+void down(float distance) {
+
+  if (button > 0) { return; }
+  digitalWrite(dirPin, LOW);    // Enables the motor to move right
+  digitalWrite(dirPin2, HIGH);  // Enables the motor to move right/HIGH left/LOW
+  stepperMotor(distance);
+  y = y - distance;
+}
+
+
+void left(float distance) {
+
+  if (button > 0) { return; }
+  digitalWrite(dirPin, LOW);   //Changes the rotations direction left
+  digitalWrite(dirPin2, LOW);  //Changes the rotations direction left
+  stepperMotor(distance);
+  x = x - distance;
 }
