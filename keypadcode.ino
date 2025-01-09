7da7aad8726e8f542986be554f161d3ba6e87467
Hamilton Dwyer <73967471+Oatzel@users.noreply.github.com>
2025-01-09 09:41:14 -0500

    Update keypadcode.ino
    This john work

--- keypadcode.ino
+++ keypadcode.ino
@@ -1,58 +1,40 @@
-#include <Wire.h>
 #include <Keypad.h>
+#include <LiquidCrystal_I2C.h>
 
+const int ROW_NUM    = 4; // four rows
+const int COLUMN_NUM = 4; // four columns
 
-const byte numRows = 4;  // Number of rows on the keypad
-const byte numCols = 4;  // Number of columns on the keypad
-
-
-// Keymap for the 4x4 keypad layout
-char keymap[numRows][numCols] = {
-  {'1', '2', '3', 'A'},
-  {'4', '5', '6', 'B'},
-  {'7', '8', '9', 'C'},
-  {'*', '0', '#', 'D'}
+char keys[ROW_NUM][COLUMN_NUM] = {
+  {'1','2','3', 'A'},
+  {'4','5','6', 'B'},
+  {'7','8','9', 'C'},
+  {'*','0','#', 'D'}
 };
 
-// Pin assignments for the keypad
-byte rowPins[numRows] = {9, 8, 7, 6};  // Rows 0 to 3
-byte colPins[numCols] = {5, 4, 3, 2};  // Columns 0 to 3
+byte pin_rows[ROW_NUM] = {9, 8, 7, 6};      
+byte pin_column[COLUMN_NUM] = {5, 4, 3, 2}; 
 
-Keypad myKeypad = Keypad(makeKeymap(keymap), rowPins, colPins, numRows, numCols);
+Keypad keypad = Keypad(makeKeymap(keys), pin_rows, pin_column, ROW_NUM, COLUMN_NUM );
+LiquidCrystal_I2C lcd(0x27, 16, 2); 
 
-String input = "";  // Store input from the keypad
+int column = 0;
 
-void setup() {
-  Serial.begin(9600);  // For debugging
-  Wire.begin();        // Initialize I2C as master
-  pinMode(LED_BUILTIN, OUTPUT);
+void setup(){
+  lcd.init(); 
+  lcd.backlight();
 }
 
-void loop() {
-  char key = myKeypad.getKey();  // Get keypress from keypad
+void loop(){
+  char key = keypad.getKey();
 
   if (key) {
-    Serial.println(key);  // Print key to Serial Monitor for debugging
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(100);
-    digitalWrite(LED_BUILTIN, LOW);
-    if (key == '#') {
-      // When "#" is pressed, send input to slave and reset input
-      sendToSlave(input);
-      input = "";  // Clear input after sending
-    }
-    else if (key == '*') {
-      // When "*" is pressed, reset the input
-      input = "";
-    }
-    else {
-      input += key;  // Add key to input string
+    lcd.setCursor(column, 0); 
+    lcd.print(key);                 
+
+    column++;                  
+    if(column == 5) {       
+      lcd.clear();
+      column = 0;
     }
   }
-}
-
-void sendToSlave(String data) {
-  Wire.beginTransmission(9);  // Address of the slave (I2C address 9)
-  Wire.write(data.c_str());   // Send the input string
-  Wire.endTransmission();     // End transmission
 }
