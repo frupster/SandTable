124941f1ad2db4a81195120a7a67b0a41f2691e2
frupster <117213809+frupster@users.noreply.github.com>
2025-01-14 23:11:23 -0500

    Update keypadcode.ino

--- keypadcode.ino
+++ keypadcode.ino
@@ -11,7 +11,7 @@ char keymap[numRows][numCols] = {
   {'4', '5', '6', 'B'},
   {'7', '8', '9', 'C'},
   {'*', '0', '#', 'D'}
-};
+}; 
 
 
 // Pin assignments for the keypad
@@ -29,39 +29,184 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   lcd.init(); 
   lcd.backlight();
+  lcd.print("Input Command:");
+  lcd.setCursor(0, 1);    
+
+
 }
 
 
 void loop() {
   char key = myKeypad.getKey();  // Get keypress from keypad
-
- 
-
+/*
     if(column == 5) {       
       lcd.clear();
       column = 0;
     }
-
+*/
   if (key) {
     Serial.println(key);  // Print key to Serial Monitor for debugging
     digitalWrite(LED_BUILTIN, HIGH);
     delay(100);
     digitalWrite(LED_BUILTIN, LOW);
-    lcd.setCursor(column, 0); 
-    lcd.print(key);                 
-    column++;    
+
     
     if (key == '#') {
       // When "#" is pressed, send input to slave and reset input
       sendToSlave(input);
       input = "";  // Clear input after sending
+       lcd.clear();
+       lcd.print("Input Command:");
+       lcd.setCursor(0, 1);
     }
+    
+    else {
+      input += key;  // Add key to input string
+    }
+
+    lcd.clear();
+    lcd.print("Input Command:");
+    lcd.setCursor(0, 1);
+    lcd.print(input);
+
+    if(input == ""){
+      lcd.clear();
+      lcd.print("Enter Command");
+      lcd.setCursor(0, 1);
+      lcd.print(input);
+    }
+
+    if(input == "1"){
+      lcd.clear();
+      lcd.print("Grow Triangle");
+      lcd.setCursor(0, 1);
+      lcd.print(input);
+    }
+    else if(input == "2"){
+      lcd.clear();
+      lcd.print("Grow Square");
+      lcd.setCursor(0, 1);
+      lcd.print(input);
+    }
+
+    else if(input == "3"){
+      lcd.clear();
+      lcd.print("Grow Polygon");
+      lcd.setCursor(0, 1);
+      lcd.print(input);
+    }
+    else if(input == "4"){
+      lcd.clear();
+      lcd.print("Rotate Triangle A/B/C/D");
+      lcd.setCursor(0, 1);
+      lcd.print(input);
+    }
+    else if(input == "4A"){
+      lcd.clear();
+      lcd.print("Rotate Triangle A");
+      lcd.setCursor(0, 1);
+      lcd.print(input);
+    }
+    else if(input == "4B"){
+      lcd.clear();
+      lcd.print("Rotate Triangle B");
+      lcd.setCursor(0, 1);
+      lcd.print(input);
+    }
+     else if(input == "4C"){
+      lcd.clear();
+      lcd.print("Rotate Triangle C");
+      lcd.setCursor(0, 1);
+      lcd.print(input);
+    }
+     else if(input == "4D"){
+      lcd.clear();
+      lcd.print("Rotate Triangle D");
+      lcd.setCursor(0, 1);
+      lcd.print(input);
+    }
+    else if(input == "5"){
+      lcd.clear();
+      lcd.print("Rotate Square A/B/C/D");
+      lcd.setCursor(0, 1);
+      lcd.print(input);
+    }
+    else if(input == "5A"){
+      lcd.clear();
+      lcd.print("Rotate Square A");
+      lcd.setCursor(0, 1);
+      lcd.print(input);
+    }
+    else if(input == "5B"){
+      lcd.clear();
+      lcd.print("Rotate Square B");
+      lcd.setCursor(0, 1);
+      lcd.print(input);
+    }
+    else if(input == "5C"){
+      lcd.clear();
+      lcd.print("Rotate Square C");
+      lcd.setCursor(0, 1);
+      lcd.print(input);
+    }
+    else if(input == "5D"){
+      lcd.clear();
+      lcd.print("Rotate Square D");
+      lcd.setCursor(0, 1);
+      lcd.print(input);
+    }
+    else if(input == "6"){
+      lcd.clear();
+      lcd.print("STEM");
+      lcd.setCursor(0, 1);
+      lcd.print(input);
+    }
+    else if(input == "7"){
+      lcd.clear();
+      lcd.print("UpDown");
+      lcd.setCursor(0, 1);
+      lcd.print(input);
+    }
+      else if(input == "8"){
+      lcd.clear();
+      lcd.print("LeftRight");
+      lcd.setCursor(0, 1);
+      lcd.print(input);
+    }
+    else if(input == "9"){
+      lcd.clear();
+      lcd.print("ZigZag");
+      lcd.setCursor(0, 1);
+      lcd.print(input);
+    }
+      else if(input == "0"){
+      lcd.clear();
+      lcd.print("Spiral");
+      lcd.setCursor(0, 1);
+      lcd.print(input);
+    }
+    
+
     else if (key == '*') {
       // When "*" is pressed, reset the input
       input = "";
+    lcd.clear();
+    lcd.print("Input Command:");
+    lcd.setCursor(0, 1);
     }
-    else {
-      input += key;  // Add key to input string
+    else if (key == '#') {
+      input = "";  // Clear input after sending
+       lcd.clear();
+       lcd.print("Input Command:");
+       lcd.setCursor(0, 1);
+    }
+    
+
+    else{
+      lcd.clear();
+      lcd.print("Invalid Command");
+      lcd.setCursor(0, 1);
+      lcd.print(input);
     }
   }
 }
